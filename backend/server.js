8349e831419ad1ad3e3f8d01109eea581aba96f4
Toby Fang <tobyfang05@gmail.com>
2025-05-13 00:46:01 +0800

    first commit

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+//const express = require('express');
+
+import express from "express";
+
+const app = express();
+
+app.listen(5000, ()=> {
+     console.log("server started at http://localhost:5000");
+})
