78bf3637ba50d3bfb4ff3961ccbfdeb4e4537c40
Toby Fang <tobyfang05@gmail.com>
2025-05-13 01:17:04 +0800

    Pre mongol setup

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,18 @@
 //const express = require('express');
 
 import express from "express";
+import dotenv from "dotenv";
 
+dotenv.config();
 const app = express();
 
-app.listen(5000, ()=> {
-     console.log("server started at http://localhost:5000");
-})
+app.get("/produts", (req, res) => {
+    
+});
+
+console.log(process.env.MONGOL_URI);
+
+app.listen(3000, ()=> {
+     console.log("server started at http://localhost:3000");
+});
+
