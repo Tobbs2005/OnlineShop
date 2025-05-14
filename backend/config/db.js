71cf252d38746a73d6fbf471bfd179325119b873
Toby Fang <tobyfang05@gmail.com>
2025-05-14 23:41:05 +0800

    small update

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,6 +1,7 @@
 export const connectDB = async () => {
     try {
-        
+        const conn = await mongoose.connect(process.env.MONGOL_URI);
+        console.log(`MongoDB connected: ${conn.connection.host}`);
     } catch (error) {
 
     }
